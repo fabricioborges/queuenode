0ae1972c03409ba1e84f5b07b7292846bda7a603
Fabricio <fabricio.riff2@gmail.com>
2020-05-19 21:43:36 -0300

    criado primeiro job e fila utilizando redis

---
+++ src/app/jobs/RegistrationMail.js
@@ -0,0 +1,15 @@
+import Mail from '../lib/Mail';
+
+export default {
+    key: 'RegistrationMail',
+    async handle({ data }) {
+        const {user} = data;
+        
+        await Mail.sendMail({
+            from: 'Queue Teste <queue@queuetest.com.br>',
+            to: `${user.name} <${user.email}>`,
+            subject: 'Cadastro de usuários',
+            html: `Olá, ${user.name}, bem-vindo ao sistemas de filas : D`
+        });
+    }
+}
