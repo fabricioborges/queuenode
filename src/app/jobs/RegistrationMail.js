9d22d932284fba7b323d748c81437d920fde54cd
Fabricio <fabricio.riff2@gmail.com>
2020-05-20 20:30:55 -0300

    finalizando aplicação de jobs

--- src/app/jobs/RegistrationMail.js
+++ src/app/jobs/RegistrationMail.js
@@ -2,9 +2,12 @@ import Mail from '../lib/Mail';
 
 export default {
     key: 'RegistrationMail',
+    options: {
+        delay: 5000,
+    },
     async handle({ data }) {
         const {user} = data;
-        
+
         await Mail.sendMail({
             from: 'Queue Teste <queue@queuetest.com.br>',
             to: `${user.name} <${user.email}>`,
